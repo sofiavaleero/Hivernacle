99fefcca97a961e7a305305aa9e6bb8e697377e5
Gisela Leon Pipo <gisela.leon@estudiantat.upc.edu>
2023-11-10 18:51:44 +0100

    holita

--- Hivernacle/src/main.cpp
+++ Hivernacle/src/main.cpp
@@ -1,18 +1,54 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Configura tus credenciales de WiFi
+const char *ssid = "iPhone de Paula";
+const char *password = "f472547F";
+
+// Configura los pines de los sensores
+#define DHT_PIN 4      // Pin de datos del sensor DHT11
+const int ldrPin = A0; // Pin del sensor LDR
+
+DHT dht(DHT_PIN, DHT11);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+
+  // Conéctate a la red WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando a WiFi...");
+  }
+  Serial.println("Conectado a la red WiFi");
+
+  // Inicializa el sensor DHT11
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Lee la temperatura y la humedad
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Lee el valor del sensor LDR
+  int ldrValue = analogRead(ldrPin);
+
+  // Imprime los valores
+  Serial.print("Temperatura: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  Serial.print("Humedad: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  Serial.print("Valor del sensor LDR: ");
+  Serial.println(ldrValue);
+
+  // Espera 10 segundos antes de realizar la próxima lectura
+  delay(10000);
 }
